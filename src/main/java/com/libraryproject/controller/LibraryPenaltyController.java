79b84fb23139c159f110125519d4a247c7d21cd2
Mike-2002-mx <mscarlxrdm@gmail.com>
2025-06-20 22:07:49 -0600

    Remove code repeat

--- src/main/java/com/libraryproject/controller/LibraryPenaltyController.java
+++ src/main/java/com/libraryproject/controller/LibraryPenaltyController.java
@@ -37,7 +37,6 @@ public class LibraryPenaltyController {
     @Autowired
 	private LibraryPenaltyService service;
 
-	//GET All Library Penalities
 	@Operation(summary="Get all library penalities.")
     @ApiResponse(responseCode="200", description="Found library penalities.", content={
         @Content(mediaType="application/json", array=@ArraySchema(schema=@Schema(implementation=LibraryPenalty.class)))   
@@ -47,27 +46,19 @@ public class LibraryPenaltyController {
 		return service.getAll();
 	}
 
-	//GET One Registry (Library Penality)
 	@Operation(summary = "Get a library penality registry through Id.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Found penality registry.", content = @Content(schema = @Schema(implementation = LibraryPenalty.class))),
             @ApiResponse(responseCode = "400", description = "Bad request.", content = @Content(schema = @Schema(implementation = ExceptionHandlerAdvice.class))),
 			@ApiResponse(responseCode = "404", description = "The stock registry requested doesn't exists.", content = @Content(schema = @Schema(implementation = ExceptionHandlerAdvice.class))),
     })
-		//For use with MongoDB:
-	// @GetMapping("{id}")
-	// public ResponseEntity<LibraryPenalty> getByIdLibraryPenalty(@PathVariable Integer id) {
-	// 	LibraryPenalty libraryPenalty = service.getByIdLibraryPenalty(id);
-	// 	return new ResponseEntity<LibraryPenalty>(libraryPenalty, HttpStatus.OK);
-	// }
-		//For use with MySQL:
+
 	@GetMapping("{idLibraryPenalty}")
 	public ResponseEntity<LibraryPenalty> getByIdLibraryPenalty(@PathVariable int idLibraryPenalty) {
 		LibraryPenalty libraryPenalty = service.getByIdLibraryPenalty(idLibraryPenalty);
 		return new ResponseEntity<>(libraryPenalty, HttpStatus.OK);
 	}
 
-	//POST New Registry (Library Penality)
 	@Operation(summary = "Add new library penality registry.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Successfully registered library penality.", content = @Content(schema = @Schema(implementation = LibraryPenalty.class))),
@@ -78,22 +69,13 @@ public class LibraryPenaltyController {
 		service.save(libraryPenalty);
 	}
 
-	//PUT Update a Library Penality registry
 	@Operation(summary = "Update the library penality")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Successfully updated library penality.",content = @Content(schema = @Schema(implementation = LibraryPenalty.class))),
 			@ApiResponse(responseCode = "400", description = "Bad request.", content = @Content(schema = @Schema(implementation = ExceptionHandlerAdvice.class))),
             @ApiResponse(responseCode = "404", description = "The library penality registry requested doesn't exists.",content = @Content(schema = @Schema(implementation = ExceptionHandlerAdvice.class)))
     })
-		//For use with MongoDB:
-	// @PutMapping("{id}")
-	// public ResponseEntity<?> update(@RequestBody LibraryPenalty libraryPenalty, @PathVariable Integer id) {
-	// 	LibraryPenalty auxLibraryPenalty = service.getByIdLibraryPenalty(id);
-	// 	libraryPenalty.setId(auxLibraryPenalty.getId());
-	// 	service.save(libraryPenalty);
-	// 	return new ResponseEntity<String>("Updated Record", HttpStatus.OK);
-	// }
-		//For use with MySQL:
+
 	@PutMapping("{idLibraryPenalty}")
 	public ResponseEntity<?> update(@RequestBody LibraryPenalty libraryPenalty, @PathVariable int idLibraryPenalty) {
 		LibraryPenalty auxLibraryPenalty = service.getByIdLibraryPenalty(idLibraryPenalty);
@@ -102,19 +84,13 @@ public class LibraryPenaltyController {
 		return new ResponseEntity<>("Updated Record", HttpStatus.OK);
 	}
 
-	//DELETE Library Penality
 	@Operation(summary = "Delete library penality")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "This library penality registry was removed from the database.", content = @Content(schema = @Schema(implementation = LibraryPenalty.class))),
             @ApiResponse(responseCode = "400", description = "Bad request.", content = @Content(schema = @Schema(implementation = ExceptionHandlerAdvice.class))),
 			@ApiResponse(responseCode = "404", description = "The library penality registry requested doesn't exists.", content = @Content(schema = @Schema(implementation = ExceptionHandlerAdvice.class)))
     })
-		//For use with MongoDB:
-	// @DeleteMapping("{id}")
-	// public void delete(@PathVariable Integer id) {
-	// 	service.delete(id);
-	// }
-		//For use with MySQL:
+
     @DeleteMapping("{idLibraryPenalty}")
 	public void delete(@PathVariable int idLibraryPenalty) {
 		service.delete(idLibraryPenalty);
